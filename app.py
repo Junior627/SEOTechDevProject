36cb6f9748d404dda13032bb4518ee464b44e15e
Kaleb <kaleb.m.tefera@gmail.com>
2023-07-27 21:47:25 +0300

    text displayed using flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def homepage():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
