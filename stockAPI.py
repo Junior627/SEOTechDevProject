dda07818ca8d3dbcf059645d08650d4c46a4ee1c
Kaleb <kaleb.m.tefera@gmail.com>
2023-08-01 10:52:14 +0300

    added class variables, return date object lists

--- stockAPI.py
+++ stockAPI.py
@@ -5,40 +5,51 @@ import constants
 
 apikey = constants.api_key
 timezone = "America/New_York"
-order = "asc"
 class stockAPI:
     def __init__(self, company, interval, outputsize, startDate, endDate):
-        global payload 
-        payload = {
-                'start_date':startDate, # accepts yyyy-MM-dd or yyyy-MM-dd hh:mm:ss
+        self.oneRunTime = False
+        self.dropDates = {}    
+        self.belowInflationDates = {}
+        self.tenIncreaseDates = {}
+        self.orderedData = {}
+        self.dataDateAverage = {}
+        self.payload = {
+                'start_date':startDate,
                 'end_date': endDate,
                 'symbol':company, 
-                'interval':interval, # accepts 1min, 5min, 15min, 30min, 45min, 1h, 2h, 4h, 8h, 1day, 1week, 1month
-                'outputsize':outputsize, # int
-                'timezone':timezone, # UTC, Exchange, IANA TimeZone Database
-                'apikey':apikey,
-                'order':order
+                'interval':interval, 
+                'outputsize':outputsize,
+                'timezone':timezone, 
+                'apikey':apikey
                 }
         
     def requestAPIData(self):
         global r 
-        r = requests.get('https://api.twelvedata.com/time_series', params=payload)
+        r = requests.get('https://api.twelvedata.com/time_series', params=self.payload)
 
     def getDateAndAverage(self):
         # average is x while date is y
         rawdata = r.json()
-        global dataDateAverage
-        dataDateAverage = {}
         for datapoint in rawdata['values']:
             average = (float(datapoint['high'])+ float(datapoint['low']))/2
             average = round(average, 8)
-            dataDateAverage[datapoint['datetime']] = average
-        return dataDateAverage
+            self.dataDateAverage[datapoint['datetime']] = average
+        return self.dataDateAverage
 
     def printdata(self):
         print("Data")
         print(" ")
         print(json.dumps(r.json(), indent=1))
+
+    def reorderDict(self, dict):
+        if (self.oneRunTime == True):
+            return dict
+        newDict = {}
+        for index in range(len(dict)):
+            item = dict.popitem()
+            newDict[item[0]] =item[1]
+        self.oneRunTime = True
+        return newDict
 
     def compareDate(self, date1, date2):
         newDate1 = datetime.strptime(date1, "%Y-%m-%d")
@@ -48,60 +59,61 @@ class stockAPI:
         return False
 
     def significantChange(self):
-        global dropDates, belowInflationDates, tenIncreaseDates, orderedData    
-        dropDates = {}    
-        belowInflationDates = {}
-        tenIncreaseDates = {}
-        orderedData = {}
-        if (len(orderedData) == 0):
+        self.orderedData = self.reorderDict(self.dataDateAverage)
+        if (len(self.orderedData) == 0):
             self.getDateAndAverage()
-        for items in orderedData.items():
-            if (items[0] == list(orderedData.keys())[-1]):
+        for items in self.orderedData.items():
+            if (items[0] == list(self.orderedData.keys())[-1]):
                 continue
-            if (items[0] == list(orderedData.keys())[0]):
+            if (items[0] == list(self.orderedData.keys())[0]):
                 previouskey = items[0]
                 previousValue = items[1]
                 continue
             # stock got lower in value
             if (items[1] < previousValue):
-                dropDates[previouskey] = previousValue
+                self.dropDates[previouskey] = previousValue
             relativeChange = ((items[1]-previousValue)/previousValue)
             # stock growth is lower than inflation
             if (relativeChange < 0.038):
-                belowInflationDates[previouskey] = previousValue
+                self.belowInflationDates[previouskey] = previousValue
             # stock growth greater than 10 percent
             if (relativeChange > 0.1):
-                tenIncreaseDates[previouskey] = previousValue
+                self.tenIncreaseDates[previouskey] = previousValue
 
             previouskey = items[0]
             previousValue = items[1]
         
-        return dropDates
+        return self.dropDates
     def getOrderedDate(self):
-        if (len(orderedData) == 0):
+        if (len(self.orderedData) == 0):
             self.significantChange()
-        return orderedData
-    def getDropDates(self):
-        return dropDates
+        return self.orderedData
+    
+    def parseDates(self, dict):
+        dateList = list(dict.keys())
+        for index in range(len(dateList)):
+            dateList[index] = datetime.strptime(dateList[index], "%Y-%m-%d")
+        return dateList
+
+
+    def getdropDates(self):
+        return self.parseDates(self.dropDates)
     
     def getDropInflationDates(self):
-        return belowInflationDates
+        return self.belowInflationDates
     
     def gettenincreaseDates(self):
-        return tenIncreaseDates
+        return self.tenIncreaseDates
+    
 
-apple = stockAPI("AAPL", "1day", 100, "2019-01-01", "2019-02-01")
-apple.requestAPIData()
-dataset = apple.getDateAndAverage()
-print(dataset)
-'''    
 #Running Class Example
 
-
+apple = stockAPI("AAPL", "1month", 100, "2015-01-01", "2020-01-01")
+apple.requestAPIData()
+apple.getDateAndAverage()
 apple.significantChange()
 print("Stock dropped in value \n")
-print(apple.getDropDates())
-
+print(apple.getdropDates()[0].year)
 print("\n Stock growth is below Inflation \n")
 print(apple.getDropInflationDates())
 
@@ -110,4 +122,3 @@ print(apple.gettenincreaseDates())
 
 print("\n Printing entire set \n")
 print(apple.getOrderedDate())
-'''
