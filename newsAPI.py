8b809e1c919262090bb79d49a1e4872981b4882b
Junior627 <43441842+Junior627@users.noreply.github.com>
2023-08-03 10:55:13 -0400

    Updated newsAPI.py

--- newsAPI.py
+++ newsAPI.py
@@ -1,33 +1,41 @@
 from flask import Flask, render_template
-from constants import news_api_key
 import requests
 import random
 import pprint
+import constants
 
-
-SECRET_KEY = news_api_key 
+SECRET_KEY = constants.news_key 
 symbols = ["TSLA", "AAPL", "MSFT", "GOOG", "AMZN", "NVDA"]
 months  = ["01", "02" ,"03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
 years = ["2021", "2022"]
 bound = 99
 newsArray = []
-        
-def retrieveDateCompany():
-    return [symbol, month, year]
+symbol=""
+Month=""
+nextMonth=""
+Year=""
+nextYear=""
+Day="01"
 
 def requestInfo():
-    global newsArray, symbol, month, year
-    symbol = randomSelector(symbols)
-    month = randomSelector(months)
-    year = randomSelector(years)
+    global newsArray 
     newsArray = []
-    print(year)
-    url = f"https://api.marketaux.com/v1/news/all?symbols={symbol}&interval=month&published_before={year}-{month}&filter_entities=true&language=en&api_token={SECRET_KEY}"
+
+    global symbol
+    symbol = randomSelector(symbols)
+    global Month
+    Month = randomSelector(months)
+    global Year
+    Year = randomSelector(years)
+    getNextDate(Month)
+    
+    print(Year)
+    url = f"https://api.marketaux.com/v1/news/all?symbols={symbol}&interval=month&published_before={Year}-{Month}-{Day}&filter_entities=true&language=en&api_token={SECRET_KEY}"
     response = requests.get(url)
     news = response.json()
     for x in news["data"]:
         newsArray.append(x)
-    
+        
 
 def printNews():
     global newsArray
@@ -35,9 +43,27 @@ def printNews():
         print(x['title']+"\n")
         print(x['description']+"\n")
 
+def getNextDate(string):
+    Day = "01"
+    if string =="12":
+        global nextMonth
+        nextMonth = "01"
+        if Year =="2021":
+            global nextYear
+            nextYear = "2022"
+        elif Year=="2022":
+            Day = "30"
+        
+    else:
+        i = 0
+        nextYear = Year
+        while i < len(months):
+            if string == months[i]:
+                nextMonth = months[i+1]
+            i = i + 1
+
 def randomSelector(arr):
     return arr[random.randrange(0, len(arr))]
-
 
 
 
